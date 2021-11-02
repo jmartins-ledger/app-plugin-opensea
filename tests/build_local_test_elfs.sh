0886ca7df07cc4c43526ae0543332a0b9ea92273
Enzo Piriou <enzopiriou@gmail.com>
2021-11-02 20:06:29 +0100

    add TESTIN_KEY=1 to make app-ethereum

--- tests/build_local_test_elfs.sh
+++ tests/build_local_test_elfs.sh
@@ -21,7 +21,7 @@ cp bin/app.elf "tests/elfs/plugin_nanos.elf"
 # echo "**Building app-ethereum for Nano S..."
 # cd $APP_ETHEREUM
 # make clean BOLOS_SDK=$NANOS_SDK
-# make -j DEBUG=1 BYPASS_SIGNATURES=1 BOLOS_SDK=$NANOS_SDK CHAIN=ethereum
+# make -j DEBUG=1 BYPASS_SIGNATURES=1 BOLOS_SDK=$NANOS_SDK CHAIN=ethereum TESTING_KEY=1
 # cd -
 # cp "${APP_ETHEREUM}/bin/app.elf" "tests/elfs/ethereum_nanos.elf"
 
