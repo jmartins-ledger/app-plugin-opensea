30d4c0d7133801fc4ba03b6b206aa17e2e7ac7a1
PGlenn1 <piriou.glenn@gmail.com>
2022-03-07 00:25:51 +0100

    v2 tests updated, cancelOrder_noTokenID to be fixed

--- tests/src/increment_nonce.test.js
+++ tests/src/increment_nonce.test.js
@@ -4,7 +4,7 @@ import { waitForAppScreen, zemu, txFromEtherscan, boilerplateJSON, resolutionCon
 import ledgerService from "@ledgerhq/hw-app-eth/lib/services/ledger"
 
 // https://etherscan.io/tx/0x72937bfe0b748d852bd0fa326b1000a42bc307a31485b6d9259a786efc82c29c
-test('[Nano S] increment_nonce', zemu("nanos", async (sim, eth) => {
+test('[Nano S] nanos_increment_nonce', zemu("nanos", async (sim, eth) => {
     const serializedTx = txFromEtherscan("0x02f8700181d08459682f0085164810f06b82b099947f268357a8c2552623316e2562d90e642bb538e58084627cdcb9c001a0cbeb8887a4fde5855087183d841997b89878c8bcb77a6b7ce43018def8aabdd8a0778979e7afc3753c80c4a0df98232bde9493ce7f73c70a19c4131ffa6f1ec5df");
     const resolution = await ledgerService.resolveTransaction(
       serializedTx,
@@ -17,6 +17,6 @@ test('[Nano S] increment_nonce', zemu("nanos", async (sim, eth) => {
       resolution
     );
     await waitForAppScreen(sim);
-    await sim.navigateAndCompareSnapshots('.', 'increment_nonce', [8, 0]);
+    await sim.navigateAndCompareSnapshots('.', 'nanos_increment_nonce', [8, 0]);
     await tx;
   }));
